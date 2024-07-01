c30eb4dd7224c1d08666e73bfb67de09b0b1dcbf
MusaTamzid05 <tamzid101@gmail.com>
2024-07-01 22:20:53 +0600

    Task Creation

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from account.models import Profile
+from .forms import TaskForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def create_task(request):
+    profile = Profile.objects.get(user=request.user)
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.profile = profile
+            task.save()
+            return redirect("dashboard")
+    else:
+        form = TaskForm()
+
+    return render(request, "tasks/create_task.html", {"form" : form})
+
+
+
