867de6353f66dd29bb1bbf10e6eedbdab776e177
MusaTamzid05 <tamzid101@gmail.com>
2024-07-01 21:36:09 +0600

    Create task app with model

---
+++ tasks/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from account.models import Profile
+
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+            ("TO_DO", "To Do"),
+            ("IN_PROGRESS", "In Progress"),
+            ("DONE", "Done")
+            ]
+
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=2000)
+    description = models.TextField()
+    due_date = models.DateField()
+    status = models.CharField(
+            max_length=20,
+            choices=STATUS_CHOICES,
+            default="TO_DO"
+            )
+
+    def __str__(self):
+        return self.title
--- todo/settings.py
+++ todo/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "tasks.apps.TasksConfig",
 ]
 
 MIDDLEWARE = [
