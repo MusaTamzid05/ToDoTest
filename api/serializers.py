01a3c89d59dbc40af7c09dd737040bd330c3f234
MusaTamzid05 <tamzid101@gmail.com>
2024-07-02 17:35:02 +0600

    Send celery notfication in rest

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from tasks.models import Task
 from tasks.models import Profile
 from django.contrib.auth.models import User
+from tasks.celery_tasks import send_celery_task_notification
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -24,3 +25,14 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ["id", "title", "description", "due_date", "status", "profile", "profile_id"]
+
+    def create(self, validated_data):
+        task = Task.objects.create(**validated_data)
+        send_celery_task_notification.delay(task_id=task.id, created=True)
+        return task
+
+
+    def update(self, instance, validated_data):
+        task = super().update(instance, validated_data)
+        send_celery_task_notification(task_id=task.id, created=False)
+        return task
