900a777761e424d2ac020b3abfd6e5cbd3e027df
MusaTamzid05 <tamzid101@gmail.com>
2024-07-02 20:51:45 +0600

    Add test for Unit Model

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils import timezone
 from account.models import Profile
 
 
--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,65 @@
 from django.test import TestCase
+from .models import Task, Profile
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class TaskModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.profile = Profile.objects.create(user=self.user)
+        self.task1 = Task.objects.create(
+            title='Task 1',
+            description='This is the first task',
+            due_date='2023-06-30',
+            status='TO_DO',
+            profile=self.profile
+        )
+        self.task2 = Task.objects.create(
+            title='Task 2',
+            description='This is the second task',
+            due_date='2023-07-15',
+            status='IN_PROGRESS',
+            profile=self.profile
+        )
+
+    def test_create_task(self):
+        task = Task.objects.create(
+            title='New Task',
+            description='This is a new task',
+            due_date='2023-08-01',
+            status='TO_DO',
+            profile=self.profile
+        )
+        self.assertEqual(task.title, 'New Task')
+        self.assertEqual(task.description, 'This is a new task')
+        self.assertEqual(task.due_date, '2023-08-01')
+        self.assertEqual(task.status, 'TO_DO')
+        self.assertEqual(task.profile, self.profile)
+        self.assertEqual(Task.objects.count(), 3)
+
+    def test_read_task(self):
+        task = Task.objects.get(id=self.task1.id)
+        self.assertEqual(task.title, 'Task 1')
+        self.assertEqual(task.description, 'This is the first task')
+        self.assertEqual(str(task.due_date), '2023-06-30')
+        self.assertEqual(task.status, 'TO_DO')
+        self.assertEqual(task.profile, self.profile)
+
+    def test_update_task(self):
+        task = Task.objects.get(id=self.task1.id)
+        task.title = 'Updated Task'
+        task.description = 'This task has been updated'
+        task.due_date = '2023-07-20'
+        task.status = 'IN_PROGRESS'
+        task.save()
+
+        updated_task = Task.objects.get(id=self.task1.id)
+        self.assertEqual(updated_task.title, 'Updated Task')
+        self.assertEqual(updated_task.description, 'This task has been updated')
+        self.assertEqual(str(updated_task.due_date), '2023-07-20')
+        self.assertEqual(updated_task.status, 'IN_PROGRESS')
+        self.assertEqual(updated_task.profile, self.profile)
+
+    def test_delete_task(self):
+        task = Task.objects.get(id=self.task1.id)
+        task.delete()
+        self.assertEqual(Task.objects.count(), 1)
