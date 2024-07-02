8c1e3d232b54671987b5c1c533b229f464de4c59
MusaTamzid05 <tamzid101@gmail.com>
2024-07-02 11:10:46 +0600

    Email  is being send

--- tasks/apps.py
+++ tasks/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save
 
 
 class TasksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tasks'
+
+    def ready(self):
+        from .signals import send_task_notification
+        post_save.connect(send_task_notification, sender="tasks.Task")
+        
---
+++ tasks/signals.py
@@ -0,0 +1,23 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.core.mail import send_mail
+from .models import Task
+
+
+@receiver(post_save, sender=Task)
+def send_task_notification(sender, instance, created, **kwargs):
+    subject = f"Task {'Created' if created else 'Updated'} : {instance.title}"
+    message = f"A New task has been {'created' if created else 'Updated'}"
+
+    print("New post save")
+
+    profile = instance.profile
+
+    send_mail(
+            subject=subject,
+            message=message,
+            from_email="noreply@email.com",
+            recipient_list=[profile.user.email],
+            fail_silently=False,
+            )
+
